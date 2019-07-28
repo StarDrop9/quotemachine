e9dcfed3db2f442cae66c2a0eadc69ce2ab7448f
StarDrop9 <kurtstarck@yahoo.com>
2019-07-28 18:58:16 -0500

    8th

--- src/components/quoteapp.js
+++ src/components/quoteapp.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './quoteapp.css';
+import './quoteapp.scss';
 
 const API = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 var randomNum = 1
 
 function Quoteapp() {
   const [data, setData] = useState({ quotes: [] });
-     
+  const [x, setX] = useState({x :{} }) ;  
+  
   useEffect(() => {
         const fetchData = async () => {
           const result = await axios(
@@ -24,28 +25,25 @@ function Quoteapp() {
         if(e.target.getAttribute("id") === "new-quote"){
           randomNum = (Math.floor(Math.random() * data.length) || 0 )
           var x = data[randomNum]
-          console.log(x) 
-           // if (typeof x === 'object'){console.log(x.quote)}
-           // if (typeof x === 'object'){console.log(x.author)} 
-            let text = document.getElementById("text");
-            let author = document.getElementById("author");
-            text.innerText = x.quote;
-            author.innerText = "_"+ x.author;
-             }
+          setX(data[randomNum])
+          let text = document.getElementById("text");
+          let author = document.getElementById("author");
+          text.innerText = x.quote;
+          author.innerText = "_"+ x.author;
+          }
        }     
- 
- return (
+       // if (typeof y === 'object'){var a = y.author} 
+return (
         <div id="wrapper">
         <div id="quote-box">
           <div className="quote-text">
-            <i className="quote"><span><i id="text" className="fa fa-quote-left" aria-hidden="true">The knowledge of the secrets of the kingdom of heaven has been given to you, but not to them. Whoever has will be given more, and he will have an abundance. Whoever does not have, even what he has will be taken from him. This is why I speak to them in parables: 
-Though seeing, they do not see; though hearing, they do not hear or understand.</i></span></i>
+            <i className="quote"><span><i id="text" className="fa fa-quote-left" aria-hidden="true">Ask and it will be given to you; search, and you will find; knock and the door will be opened for you.</i></span></i>
           </div>
           <div className="quote-author">
              <span id="author">Jesus</span>
           </div>
           <div className="buttons">
-             <a id="tweet-quote" className="tweetbutton" onClick={myHandleClick} href={"https://twitter.com/intent/tweet?hashtags="+{}} target="_blank" rel="noopener noreferrer"> <i className="fa fa-twitter"></i></a>
+             <a id="tweet-quote" className="tweetbutton" onClick={myHandleClick} href={"https://twitter.com/intent/tweet?hashtags="+x.author+"&text="+x.quote} target="_blank" rel="noopener noreferrer"> <i className="fa fa-twitter"></i></a>
             <button id="new-quote" className="btnx" onClick = {myHandleClick} >New Quote</button>
           </div>
         </div>
